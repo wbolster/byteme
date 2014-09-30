853dd0c86a0690bf728425500be75c8d4f4dcedb
Wouter Bolsterlee <uws@xs4all.nl>
2014-09-30 17:20:48 +0200

    Add empty 'byteme' package

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='byteme',
     version='0.1',
     author="Wouter Bolsterlee",
     author_email="uws@xs4all.nl",
+    packages=find_packages(),
 )

314d89d5a1ca1f86c554b898c34f1b7646e5a659
Wouter Bolsterlee <uws@xs4all.nl>
2014-10-01 19:27:37 +0200

    Add some more tests

--- byteme/leb128.py
+++ byteme/leb128.py
@@ -62,9 +62,12 @@ def leb128_decode(value, signed=False):
     return decoded, size
 
 
-def test_leb128():
+def test_leb128_known_values():
 
     test_vectors = [
+        ('00', 0x00, 0x00),
+        ('01', 0x01, 0x01),
+
         # From https://github.com/Medium/leb
         ('10', 0x10, 0x10),
         ('45', 0x45, -0x3b),
@@ -76,6 +79,12 @@ def test_leb128():
         # From http://en.wikipedia.org/wiki/LEB128
         ('e5 8e 26', 624485, None),
         ('9b f1 59', None, -624485),
+
+
+        # From http://events.linuxfoundation.org/sites/events/
+        #      files/slides/ABS2014.pdf
+        ('7f', 127, -1),
+        ('807f', 16256, -128),
     ]
 
     for s, positive, negative in test_vectors:
@@ -93,5 +102,19 @@ def test_leb128():
             # assert decoded == negative
             # assert len(b) == size
 
+
+def test_leb128_roundtrip():
+    for i in range(-123456789, 123456789, 100000):
+        if i >= 0:
+            encoded = leb128_encode(i)
+            decoded, _ = leb128_decode(encoded)
+            assert decoded == i
+
+        encoded = leb128_encode(i, signed=True)
+        # decoded, _ = leb128_decode(encoded, signed=True)
+        # assert decoded == i
+
+
+def test_leb128_trailing_bytes():
     # It should ignore trailing bytes after the terminating byte
     assert leb128_decode(b'\xe5\x8e\x26\xab\xab\xab') == (624485, 3)

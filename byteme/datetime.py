6e6408847219dd6bb341fc501bb41ffe000bdb56
Wouter Bolsterlee <uws@xs4all.nl>
2014-10-05 01:10:31 +0200

    Initial datetime module with some stubs

---
+++ byteme/datetime.py
@@ -0,0 +1,34 @@
+"""
+Date/time encoding routines.
+"""
+
+# http://dev.mysql.com/doc/internals/en/
+#    date-and-time-data-type-representation.html
+#
+#  1 bit  sign           (1= non-negative, 0= negative)
+# 17 bits year*13+month  (year 0-9999, month 0-12)
+#  5 bits day            (0-31)
+#  5 bits hour           (0-23)
+#  6 bits minute         (0-59)
+#  6 bits second         (0-59)
+# ---------------------------
+# 40 bits = 5 bytes
+#
+# TODO: microseconds= arg, use 8 bytes instead of 5 in that case
+
+
+def mysql_datetime_dumps(value):
+    raise NotImplementedError()
+
+
+def mysql_datetime_dump(value, fp):
+    fp.write(mysql_datetime_dumps(value))
+
+
+def mysql_datetime_loads(value):
+    raise NotImplementedError()
+
+
+def mysql_datetime_load(value, fp):
+    # TODO: handle partial reads
+    return mysql_datetime_loads(fp.read(5))
